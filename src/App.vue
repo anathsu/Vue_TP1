c1777a08e15a39b771af276e65dea2dfd39de4f8
Willian-Notebook <willian.silva@al.infnet.edu.br>
2020-10-23 20:20:46 -0300

    Mostrando a listagem com interface estilizada

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,19 @@
   <div id="app">
 
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+    <h1>Sistema de Cadastro</h1>
+
+    <div class="container">
+      <div class="div" v-bind:key="cadastro.id" v-for="cadastro in cadastros">
+        <p>Cadastro ID: {{cadastro.id}}</p>
+        <p>Descrição: {{cadastro.descricao}}</p>
+        <p v-if="cadastro.estaCompleta" id="completo">CADASTRO CONCLUÍDO</p>
+        <p v-else id="incompleto">CADASTRO INCOMPLETO</p>
+      </div>
+    </div>
+    
+
   </div>
 </template>
 
@@ -12,7 +25,25 @@ export default {
   name: 'App',
   components: {
     // HelloWorld
+  },
+  data(){
+    return {
+          cadastros: [
+            {id: 1, descricao: "Descrição 111111", estaCompleta: true},
+            {id: 2, descricao: "Descrição 222222", estaCompleta: false},
+            {id: 3, descricao: "Descrição 333333", estaCompleta: false},
+            {id: 4, descricao: "Descrição 4444444", estaCompleta: true},
+            {id: 5, descricao: "Descrição 5555555", estaCompleta: true},
+            {id: 6, descricao: "Descrição 666666", estaCompleta: false},
+          ]
+    }
+
   }
+  // filters: {
+  //   verificaComplemento: function(item){
+  //     return item.toUpperCase();
+  //   }
+  // }
 }
 </script>
 
@@ -24,5 +55,30 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.div{
+  /* border: palevioletred 1px solid; */
+  width: 300px;
+  height: 250px;
+}
+
+.container{
+  display: flex;
+  justify-content: center;
+  width: 90%;
+  flex-wrap: wrap;
+}
+
+#completo{
+  color: green;
+  font-weight: bolder;
+  font-size: 1.1em;
+}
+
+#incompleto{
+  color: orange;
+  font-weight: bolder;
+  font-size: 1.1em;
 }
 </style>
